0c4382139a578e4247d8c75fc2e18da5c4ba49b8
winnie2346 <winniedong68@gmail.com>
2025-10-08 12:32:24 -0400

    booooo

---
+++ app.js
@@ -0,0 +1,11 @@
+const birdfood = [
+  {
+    name: "yummy",
+    price: 0.01,
+    inStock: true,
+    brand: "Premium Bird Feed",
+    img: "images/uh",
+    alt: "bird food",
+  },
+];
+birdfood.forEach((food) => makeCard(food));
