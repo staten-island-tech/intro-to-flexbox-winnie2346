ef4437446f6f0ec8642f6d69983dbf4e9146e758
winnie2346 <winniedong68@gmail.com>
2025-10-10 12:31:46 -0400

    bumbum

--- the stuff/app.js
+++ the stuff/app.js
@@ -7,178 +7,20 @@ const birdfood = [
     img: "images/uh",
     alt: "bird food",
   },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
-  {
-    name: "yummy",
-    price: 0.01,
-    inStock: true,
-    brand: "Premium Bird Feed",
-    img: "images/uh",
-    alt: "bird food",
-  },
+  
 ];
 //create inject function
-function inject(item) {
+function inject(birdfood) {
   const child = document.querySelector("child");
   child.insertAdjacentHTML("afterbegin",
-      `<div class="display-child">
-      <img class="display-img" src="${item.name}"/>
-      <h2 class="display-heading">${item.price}</h2>
-      <h3 class="price">${} </h3>
+      `<div class="child">
+      <img class="child-img" src="${birdfood.img}"/>
+      <h2 class="child-heading">${birdfood.brand}</h2>
+      <h3 class="price">${birdfood.price} </h3>
     </div>`
-
     );
+
+
   //do something
   //query the container
   //using adjacent html push card into container
@@ -186,3 +28,16 @@ function inject(item) {
 product.forEach ((product)=>inject(product));
 //loop through items
 // birdfood.forEach((food) => makeCard(food));
+function addToCart (){
+  const buttons = document.querySelectorAll ("button");
+const btnArray= Array.from(buttons);
+console.log(buttons);
+btnArrayArray.forEach((btn) => btn.addEvenListener("click"), function(event))
+console.log (event.target.textContent);
+console.log (event.target.closest("display-cart").getattribute("data-title"));
+//find item in the array
+//take that object and push into cart
+}
+addToCart();//made an array
+//using for each to put array of cards on screen
+//work on add to cart(array)
