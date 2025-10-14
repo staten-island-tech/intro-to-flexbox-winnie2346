2c78c0acafc90f909acfabf39173a13506275d6f
winnie2346 <winniedong68@gmail.com>
2025-10-14 12:32:29 -0400

    ddddddddd

--- the stuff/app.js
+++ the stuff/app.js
@@ -7,37 +7,39 @@ const birdfood = [
     img: "images/uh",
     alt: "bird food",
   },
-  
 ];
 //create inject function
 function inject(birdfood) {
   const child = document.querySelector("child");
-  child.insertAdjacentHTML("afterbegin",
-      `<div class="child">
+  child.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="child">
       <img class="child-img" src="${birdfood.img}"/>
       <h2 class="child-heading">${birdfood.brand}</h2>
       <h3 class="price">${birdfood.price} </h3>
     </div>`
-    );
-
+  );
 
   //do something
   //query the container
   //using adjacent html push card into container
 }
-product.forEach ((product)=>inject(product));
+birdfood.forEach((birdfood) => inject(birdfood));
 //loop through items
 // birdfood.forEach((food) => makeCard(food));
-function addToCart (){
-  const buttons = document.querySelectorAll ("button");
-const btnArray= Array.from(buttons);
-console.log(buttons);
-btnArrayArray.forEach((btn) => btn.addEvenListener("click"), function(event))
-console.log (event.target.textContent);
-console.log (event.target.closest("display-cart").getattribute("data-title"));
-//find item in the array
-//take that object and push into cart
+function addToCart() {
+  const buttons = document.querySelectorAll("button");
+  const btnArray = Array.from(buttons);
+  console.log(buttons);
+  btnArray.forEach(
+    (btn) => btn.addEvenListener("click"),
+    function (event) {}
+  );
+  console.log(event.target.textContent);
+  console.log(event.target.closest("display-cart").getattribute("data-title"));
+  //find item in the array
+  //take that object and push into cart
 }
-addToCart();//made an array
+addToCart(); //made an array
 //using for each to put array of cards on screen
 //work on add to cart(array)
