7c719e2c4d469e238aa4c5b8a919653729b1b4b2
winnie2346 <winniedong68@gmail.com>
2025-10-28 00:54:59 -0400

    xxxxxxxxxxxxx

---
+++ index.js
@@ -0,0 +1,15 @@
+const DOMSelectors = {
+button: document.getElementById("btn"),
+name: document.querySelector("#name"),
+price: document.querySelectorAll(".price"),
+box: document.getElementById(".child"),
+};
+const EverythingBirds = [
+{
+    name: "Pigeon Feed",
+    price: 0.01,
+    inStock: true,
+    alt: "pigeons",
+
+}
+];
