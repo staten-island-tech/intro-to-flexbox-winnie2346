f659e51bee4e5923a65a9690d554524c9747bc98
winnie2346 <winniedong68@gmail.com>
2025-10-29 11:51:30 -0400

    vvvvv

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const DOMSelectors = {
-button: document.getElementById("btn"),
-name: document.querySelector("#name"),
-price: document.querySelectorAll(".price"),
-box: document.getElementById(".child"),
+  button: document.getElementById("btn"),
+  name: document.querySelector("#name"),
+  price: document.querySelectorAll(".price"),
+  box: document.getElementById(".child"),
 };
 const EverythingBirds = [
-{
+  {
     name: "Pigeon Feed",
     price: 0.01,
     inStock: true,
-    alt: "pigeons",
-
-}
+    img: "pigeon1",
+  },
 ];
