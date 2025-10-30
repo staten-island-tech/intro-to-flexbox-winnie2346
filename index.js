c05aa36f406bc98e81acc3a2aa14d5e46b727e41
winnie2346 <winniedong68@gmail.com>
2025-10-30 12:32:21 -0400

    cccccccccc

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const DOMSelectors = {
-  button: document.getElementsByClassName("btn"),
-  name: document.querySelector("name"),
-  price: document.querySelectorAll("price"),
-  box: document.getElementsByClassName("child"),
-  alt: document.getElementsByClassName("images"),
-};
+// const DOMSelectors = {
+//   button: document.getElementsByClassName("btn"),
+//   name: document.querySelector("name"),
+//   price: document.querySelectorAll("price"),
+//   box: document.getElementsByClassName("child"),
+//   alt: document.getElementsByClassName("images"),
+// };
 const EverythingBirds = [
   {
     name: "Pigeon Feed",
     price: 0.01,
     inStock: true,
-    alt: "pigeon1",
+    img: "https://m.media-amazon.com/images/I/71WBymL3qDL.jpg",
+    category: "pigeon",
   },
   {
     name: "Multivitamins for Pigeons",
     price: 0.01,
     inStock: true,
-    alt: "pigeon2",
+    img: "https://lafeber.com/wp-content/uploads/2020/09/72630-V1-TF-Pellet-Parakeet-SEP20-WEB.jpg",
+    category: "pigeon",
   },
 ];
 
@@ -26,9 +28,22 @@ function inject(EverythingBirds) {
         <h2>${EverythingBirds.name}</h2>
         <img
           class="images"
-          ${EverythingBirds.images}
+          ${EverythingBirds.img}
         />
         <h3>${EverythingBirds.price}</h3>
         </div>`);
+  inject(EverythingBirds[0]);
 }
-inject(EverythingBirds[0]);
+EverythingBirds.forEach((EverythingBirds) => inject(EverythingBirds));
+function addToCart() {
+  const buttons = document.querySelectorAll("buttons");
+  const btnArray = Array.from(buttons);
+  console.log(buttons);
+  btnArray.forEach(btn);
+  (btn) =>
+    btn.addEventListener("click", function (event) {
+      console.log(event.target.closest("child").getAttribute("name"));
+    });
+}
+products[2];
+addToCart();
